f62496847146c746dcc10630e77d4bd76e1bd565
Katie A <ktfalbin@gmail.com>
2026-04-02 16:54:38 -0400

    save

---
+++ script.js
@@ -0,0 +1,21 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+ctx.fillStyle = "#a5d1d5"
+ctx.fillRect(0, 0, 600,600)
+
+ctx.fillStyle = "#8bbb95"
+ctx.fillRect(0, 400, 600,600)
+
+ctx.fillStyle = "#000000"
+ctx.fillRect(0, 0, 20, 20)
+
+
+ctx.fillStyle = "#000000"
+ctx.fillRect(0, 100, 120, 120)
+
+ctx.fillStyle = "#000000"
+ctx.fillRect(120, 50, 120, 140)
+
+ctx.fillStyle = "#ffffff"
+ctx.fillRect(240, 40, 40, 40)
