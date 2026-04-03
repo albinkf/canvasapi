3a144f89eb1cf3131badacb7cdd1eedc1c8e422a
Katie A <ktfalbin@gmail.com>
2026-04-02 20:32:59 -0400

    drawing

--- script.js
+++ script.js
@@ -4,18 +4,124 @@ const ctx = canvas.getContext('2d');
 ctx.fillStyle = "#a5d1d5"
 ctx.fillRect(0, 0, 600,600)
 
-ctx.fillStyle = "#8bbb95"
-ctx.fillRect(0, 400, 600,600)
+ctx.fillStyle = "#8ec199"
+ctx.fillRect(0, 425, 600,600)
 
-ctx.fillStyle = "#000000"
+//ground2
+ctx.fillStyle = "#5f8668"
+ctx.fillRect(0, 536, 300, 600)
+ctx.fillStyle = "#5f8668"
+ctx.fillRect(20, 520, 225, 600)
+
+//cloud1
+ctx.fillStyle = "#ffffff"
+ctx.fillRect(410, 30, 100, 50)
+ctx.fillStyle = "#ffffff"
+ctx.fillRect(360, 70, 200, 20)
+ctx.fillStyle = "#ffffff"
+ctx.fillRect(430, 50, 100, 40)
+ctx.fillStyle = "#ffffff"
+ctx.fillRect(380, 50, 30, 40)
+ctx.fillStyle = "#ffffff"
+ctx.fillRect(455, 20, 30, 40)
+//cloud2
+ctx.fillStyle = "#ffffff"
+ctx.fillRect(305, 140, 100, 50)
+ctx.fillStyle = "#ffffff"
+ctx.fillRect(255, 180, 300, 20)
+ctx.fillStyle = "#f1f1f1"
+ctx.fillRect(325, 160, 100, 40)
+ctx.fillStyle = "#e1d6d6"
+ctx.fillRect(275, 160, 30, 40)
+ctx.fillStyle = "#ffffff"
+ctx.fillRect(350, 130, 30, 60)
+
+//leaves1-
+ctx.fillStyle = "#6d9c77"
 ctx.fillRect(0, 0, 20, 20)
 
+ctx.fillStyle = "#6d9c77"
+ctx.fillRect(0, 120, 120, 275)
 
-ctx.fillStyle = "#000000"
-ctx.fillRect(0, 100, 120, 120)
+ctx.fillStyle = "#6d9c77"
+ctx.fillRect(30, 88, 250, 300)
 
-ctx.fillStyle = "#000000"
-ctx.fillRect(120, 50, 120, 140)
+ctx.fillStyle = "#6d9c77"
+ctx.fillRect(100, 70, 100, 300)
 
-ctx.fillStyle = "#ffffff"
-ctx.fillRect(240, 40, 40, 40)
+ctx.fillStyle = "#6d9c77"
+ctx.fillRect(260, 80, 70, 130)
+
+ctx.fillStyle = "#6d9c77"
+ctx.fillRect(150, 50, 125, 45)
+
+ctx.fillStyle = "#6d9c77"
+ctx.fillRect(250, 250, 90, 125)
+ctx.fillStyle = "#6d9c77"
+ctx.fillRect(250, 320, 125, 45)
+ctx.fillStyle = "#6d9c77"
+ctx.fillRect(0, 20, 125, 100
+)
+
+//leaves2
+
+ctx.fillStyle = "#304634"
+ctx.fillRect(0,340, 300, 40)
+ctx.fillStyle = "#304634"
+ctx.fillRect(25,325, 300, 40)
+ctx.fillStyle = "#304634"
+ctx.fillRect(75, 300, 125, 40)
+ctx.fillStyle = "#304634"
+ctx.fillRect(300,340, 25, 40)
+ctx.fillStyle = "#304634"
+ctx.fillRect(130, 250, 90, 50)
+
+//trunk1
+
+ctx.fillStyle = "#513824"
+ctx.fillRect(0,382 , 85, 500)
+ctx.fillStyle = "#513824"
+ctx.fillRect(0,350, 150, 50)
+ctx.fillStyle = "#513824"
+ctx.fillRect(100,350, 200, 30)
+ctx.fillStyle = "#513824"
+ctx.fillRect(300,375, 50, 20)
+
+
+ctx.fillStyle = "#513824"
+ctx.fillRect(85,350, 20, 120)
+ctx.fillStyle = "#513824"
+ctx.fillRect(85,589, 30, 20)
+
+
+//branch
+ctx.fillStyle = "#513824"
+ctx.fillRect(130,325, 40, 50)
+ctx.fillStyle = "#513824"
+ctx.fillRect(190,150, 25, 150)
+ctx.fillStyle = "#513824"
+ctx.fillRect(200,150, 40, 15)
+ctx.fillStyle = "#513824"
+ctx.fillRect(225,120, 20, 45)
+ctx.fillStyle = "#513824"
+ctx.fillRect(155,300, 40, 25)
+
+
+
+//trunk2
+ctx.fillStyle = "#715239"
+ctx.fillRect(15,420, 15, 150)
+ctx.fillStyle = "#715239"
+ctx.fillRect(15,400, 15, 10)
+ctx.fillStyle = "#715239"
+ctx.fillRect(40,400, 15, 60)
+
+//leaves over
+ctx.fillStyle = "#304634"
+ctx.fillRect(0,350, 45, 25)
+
+//splotches
+ctx.fillStyle = "#a5d1d5"
+ctx.fillRect(100,125, 20, 25)
+ctx.fillStyle = "#a5d1d5"
+ctx.fillRect(50,225, 25, 15)
